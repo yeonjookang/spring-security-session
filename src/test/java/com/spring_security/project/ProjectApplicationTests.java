7907026bcf6e0ae8bad4cc4b83cdef958212c830
kang yeon joo <rkdduswndi@naver.com>
2023-11-13 17:09:54 +0900

    init: 프로젝트 초기화 작업

---
+++ src/test/java/com/spring_security/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_security.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
